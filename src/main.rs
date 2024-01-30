5e50389c940995d2786817afb127cb341dd25a14
Danish Kalesaran <danishkalesaran@gmail.com>
2024-01-30 18:00:47 +0800

    feat: scan left brace and right brace

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use json_parser::Scanner;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Please provide the path to json file.");
+        return;
+    }
+    let filepath = &args[1];
+    let content = std::fs::read_to_string(filepath);
+    if let Ok(source) = content {
+        let mut scanner = Scanner::from(source);
+        scanner.scan_tokens();
+    }
 }
